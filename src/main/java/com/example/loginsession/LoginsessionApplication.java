faccb0f84fcd38fa4be4b482fa97dccdb919d46e
taewon0923 <ytw0740@naver.com>
2023-03-18 17:39:10 +0900

    first commit

---
+++ src/main/java/com/example/loginsession/LoginsessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginsessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginsession/LoginsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
